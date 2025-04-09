d307189b7561f2e548b77fac6e8da91a2e0d3a6f
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2025-04-09 16:33:52 -0700

    draft poke manager

---
+++ firmware/inc/poke_manager.h
@@ -0,0 +1,60 @@
+#ifndef POKE_MANAGER_H
+#define POKE_MANAGER_H
+
+#include <hardware/timer.h>
+
+
+class PokeManager
+{
+public:
+
+    enum state_t
+    {
+        RESET,
+        ODOR_SETUP,
+        ODOR_DISPENSING_TO_EXHAUST,
+        ODOR_DELIVERY_TO_FINAL_VALVE,
+        ODOR_PRECLEAN,
+        VAC_START,
+        ODOR_PURGE,
+    };
+
+    PokeManager();
+
+    ~PokeManager();
+
+
+    void update();
+
+
+/**
+ * \brief true if a poke was detected.
+ */
+    bool poke_detected();
+
+
+
+private:
+
+/**
+ * \brief time we've been in the current state.
+ */
+    inline uint32_t state_duration_us()
+    {return time_us_32() - state_entry_time_us_;}
+
+    state_t state_;
+    uint32_t state_entry_time_us_;
+    size_t poke_count_;
+
+    // Constants
+    inline constexpr uint32_t VACUUM_CLOSE_TIME_US = 1e5;// 100'000;
+    inline constexpr uint32_t MIN_POKE_TIME_US = 10e3;
+    inline constexpr uint32_t ODOR_DELIVERY_TIME_US = 20e3; // ??
+    inline constexpr uint32_t ODOR_TRANSITION_TIME_US = 10e3;
+    inline constexpr uint32_t VAC_SETUP_TIME_US = 10e3;
+    inline constexpr uint32_t VAC_SETUP_TIME_US = 10e3;
+
+
+};
+
+#endif // POKE_MANAGER_H
---
+++ firmware/src/poke_manager.cpp
@@ -0,0 +1,87 @@
+#include <poke_manager.h>
+
+PokeManager::PokeManager()
+: state_{RESET}, poke_count_{0}
+{
+    // Nothing else to do!
+}
+
+
+PokeManager::~PokeManager()
+{
+    // TODO: implement this!
+}
+
+
+
+void PokeManager::update()
+{
+    // Update inputs.
+    // TODO: poke detection here.
+
+
+    state_t next_state{state_}; // initialize next-state to current state.
+
+    // Handling next-state logic.
+    switch {state_}
+    {
+        case RESET:
+            next_state_ = ODOR_SETUP;
+            break;
+        case ODOR_SETUP:
+            if (state_duration_us() >= VACUUM_CLOSE_TIME_US)
+                next_state_ = ODOR_DISPENSING_TO_EXHAUST;
+            break;
+        case ODOR_DISPENSING_TO_EXHAUST:
+            if (poke_detected())
+                next_state_ = ODOR_DELIVERY_TO_FINAL_VALVE;
+            break;
+        case ODOR_DELIVERY_TO_FINAL_VALVE:
+            if (state_duration_us() >= ODOR_DELIVERY_TIME_US)
+                next_state_ = ODOR_PRECLEAN;
+            break;
+        case ODOR_PRECLEAN:
+            if (state_duration_us() >= ODOR_TRANSITION_TIME_US)
+                next_state_ = VAC_START;
+            break;
+        case VAC_START:
+            if (state_duration_us() >= VAC_SETUP_TIME_US)
+                next_state_ = ODOR_PURGE;
+            break;
+        case ODOR_PURGE:
+            if (state_duration_us() >= FINAL_VALVE_ENERGIZED_TIME_US)
+                next_state_ = ODOR_PURGE;
+            break;
+        default:
+            break;
+    }
+
+    // Update how long we've been in the new state.
+    if (state_ != next_state_)
+    {
+        state_entry_time_us_ = time_us_32();
+    }
+
+    if (next_state_ == RESET)
+    {
+        deenergize_all_valves();
+        state_entry_time_us_ = time_us_32(); // Force this to happen at reset.
+    }
+    if (next_state_ == ODOR_DELIVERY_TO_FINAL_VALVE)
+    {
+        ++poke_count_;
+    }
+
+
+
+    // Handling state-and/or-input-depenendent output logic.
+    if (next_state_ == ODOR_PRECLEAN)
+    {
+        deenergize_valve(??);
+        // more stuff here.
+    }
+
+
+    // Update state:
+    state_ = next_state_;
+}
